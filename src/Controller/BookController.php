5c18220314f3f886df0467a4319c95ed180bc824
Miguel Leite <miguelleite200leite@gmal.com>
2023-03-21 11:17:11 +0100

    feat(update & delete) created feature of update and delete data

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class BookController extends AbstractController
 {
@@ -17,10 +18,18 @@ class BookController extends AbstractController
     return $this->json(['data' => $bookRepository->findAll()]);
   }
 
+  #[Route('/books/{book}', name: 'books_single', methods: ['GET'])]
+  public function single(int $book, BookRepository $bookRepository): JsonResponse
+  {
+    $book = $bookRepository->find($book);
+    if (!$book) throw $this->createNotFoundException();
+    return $this->json(['data' => $book]);
+  }
+
   #[Route('/books', name: 'books_create', methods: ['POST'])]
   public function create(Request $request, BookRepository $bookRepository): JsonResponse
   {
-    $data = $request->request->all();
+    $data = $request->toArray();
     $book = new Book();
     $book->setTitle($data['title']);
     $book->setIsbn($data['isbn']);
@@ -29,7 +38,33 @@ class BookController extends AbstractController
 
     $bookRepository->save($book, true);
     return $this->json([
-      'message' => 'Book created successfully'
+      'data' => 'Book created successfully'
     ], 201);
+  }
+
+  #[Route('/books/{book}', name: 'books_update', methods: ['PUT','PATCH'])]
+  public function update(int $book, Request $request, ManagerRegistry $doctrine, BookRepository $bookRepository): JsonResponse
+  {
+    $data = $request->toArray();
+    $book = $bookRepository->find($book);
+    $book->setTitle($data['title']);
+    $book->setIsbn($data['isbn']);
+    $book->setUpdatedAt(new \DateTimeImmutable('now', new \DateTimeZone('Africa/Luanda')));
+
+    $doctrine->getManager()->flush();
+    
+    return $this->json([
+      'data' => $book
+    ], 202);
+  }
+
+  #[Route('/books/{book}', name: 'books_delete', methods: ['DELETE'])]
+  public function delete(int $book, Request $request, BookRepository $bookRepository): JsonResponse
+  {
+    $book = $bookRepository->find($book);
+    $bookRepository->remove(entity: $book, flush: true);
+    return $this->json([
+      'success' => true
+    ]);
   }
 }
